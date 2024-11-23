b80f91b01cfdfab0950467db3d67175d7b60e54e
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-11-23 16:12:51 -0700

    Add service to get article text

---
+++ services/articleService.js
@@ -0,0 +1,34 @@
+const {JSDOM} = require("jsdom");
+const TEST_HTML = require("./testHTML.js");
+const parser = require("./utils/parser.js");
+
+async function getArticleHTML(title) {
+    const url = "https://en.wikipedia.org/w/api.php?" +
+    new URLSearchParams({
+        origin: "*",
+        action: "parse",
+        page: title,
+        format: "json",
+        prop: "text"
+    });
+  
+    const req = await fetch(url);
+    const json = await req.json();
+    const rawHtml = json.parse.text["*"]; 
+    
+    return rawHtml;
+}
+
+function getArticleText(title) {
+    // const rawHtml = await getArticleHTML(title);
+    const rawHtml = TEST_HTML;
+    console.log(`rawHtml -> ${rawHtml}`)
+    const dom = new JSDOM(rawHtml);
+    const contentElement = dom.window.document.querySelector(".mw-content-ltr");
+    const articleText = parser.parseText(contentElement);
+    console.log(`parsed string length: ${articleText.length}`);
+
+    return articleText;
+}
+
+module.exports = { getArticleHTML, getArticleText };
---
+++ services/utils/parser.js
@@ -0,0 +1,74 @@
+const ParserModule = (function () {
+    let imageCount = 0;
+
+    function isReferencesSection(node) {
+        return (
+            node.nodeType === node.ELEMENT_NODE &&
+            node.className.includes("mw-heading") &&
+            node.firstChild.nodeType === node.ELEMENT_NODE &&
+            ["References", "Citations", "Notes", "See_also"].includes(node.firstChild.getAttribute("id"))
+        );
+    }
+
+    function isDisplayNone(node) {
+        const nodeDisplayStyle = node.style?.display;
+        return nodeDisplayStyle === "none";
+    }
+
+    function isInvalidTag(node) {
+        const nodeHtmlTag = node.nodeName.toLowerCase();
+        return ["style", "script"].includes(nodeHtmlTag);
+    }
+
+    function isBracketReferences(node) {
+        return node.nodeName.toLowerCase() === "sup" && node.classList.contains("reference");
+    }
+
+    function isEditSection(node) {
+        return node.classList?.contains("mw-editsection");
+    }
+
+    function isExplorableNode(node) {
+        return !isDisplayNone(node) && !isInvalidTag(node) && !isBracketReferences(node) && !isEditSection(node);
+    }
+
+    function isImage(node) {
+        return node.nodeName.toLowerCase() === "img";
+    }
+
+    function incrementImageCount() {
+        imageCount++;
+    }
+
+    function getImageCount() {
+        return imageCount;
+    }
+
+    function parseText(node) {
+        let text = "";
+        if (node.nodeType === node.TEXT_NODE) {
+            text += node.textContent;
+        } else if (node.nodeType === node.ELEMENT_NODE) {
+            if (isImage(node)) {
+                incrementImageCount();
+            }
+            for (let childNode of node.childNodes) {
+                if (isReferencesSection(childNode)) {
+                    break;
+                }
+                if (isExplorableNode(childNode)) {
+                    text += " " + parseText(childNode);
+                }
+            }
+        }
+        return text;
+    }
+
+    return {
+        parseText,
+        getImageCount
+    };
+})();
+
+
+module.exports = ParserModule;
