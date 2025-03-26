3c6874fa3924145bad91d76dcca616166e38dd79
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-03-25 19:47:23 -0600

    Connect to a cloud redis instance

--- cache/redisClient.js
+++ cache/redisClient.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const redis = require("redis");
-const client = redis.createClient();
 
-client.on("error", (err) => {
-  console.error("Redis error:", err);
+const client = redis.createClient({
+    url: process.env.REDIS_URL
 });
 
-client.connect();
+client.on("error", (err) => {
+    console.error("❌ Redis error:", err);
+  });
+  
+client.connect()
+    .then(() => console.log("✅ Connected to Redis Cloud"))
+    .catch((err) => console.error("❌ Redis connect error:", err));
 
 module.exports = client;
