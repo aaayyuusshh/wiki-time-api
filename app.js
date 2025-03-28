b5dffbb7e5ef2ffcc3f203478b2b43d220cd9782
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-03-27 23:11:25 -0600

    Add rate limiting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const articleRoutes = require("./routes/articleRoutes");
 const errorHandler = require("./middlewares/errorHandler");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,17 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`🎧 server listening on port ${PORT}`);
 })
+
+// basic rate limiter: 100 requests per 15 minutes per IP
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// apply limiter to all requests
+app.use(limiter);
 
 // root route
 app.get("/", (req, res) => {
